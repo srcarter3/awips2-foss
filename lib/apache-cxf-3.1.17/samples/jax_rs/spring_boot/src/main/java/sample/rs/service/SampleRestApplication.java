893125c6e6642d0f32850763be9cbe19cbc2e2c9
George Crawley <george.crawley@noaa.gov>
2018-11-07 17:36:42 -0500

    DR 20998 Monthly Security patch updates for Apache CXF and Apache Commons-SSL

---
+++ lib/apache-cxf-3.1.17/samples/jax_rs/spring_boot/src/main/java/sample/rs/service/SampleRestApplication.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package sample.rs.service;
+import java.util.Arrays;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import sample.rs.service.hello1.HelloServiceImpl1;
+import sample.rs.service.hello2.HelloServiceImpl2;
+
+@SpringBootApplication
+public class SampleRestApplication {
+    @Autowired
+    private Bus bus;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleRestApplication.class, args);
+    }
+ 
+    @Bean
+    public Server rsServer() {
+        JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
+        endpoint.setBus(bus);
+        endpoint.setServiceBeans(Arrays.<Object>asList(new HelloServiceImpl1(), new HelloServiceImpl2()));
+        endpoint.setAddress("/");
+        endpoint.setFeatures(Arrays.asList(createSwaggerFeature()));
+        return endpoint.create();
+    }
+
+    public Swagger2Feature createSwaggerFeature() {
+        Swagger2Feature swagger2Feature = new Swagger2Feature();
+        swagger2Feature.setPrettyPrint(true);
+        swagger2Feature.setTitle("Spring Boot CXF REST Application");
+        swagger2Feature.setContact("The Apache CXF team");
+        swagger2Feature.setDescription("This sample project demonstrates how to use CXF JAX-RS services"
+                + " with Spring Boot. This demo has two JAX-RS class resources being"
+                + " deployed in a single JAX-RS endpoint.");
+        swagger2Feature.setVersion("1.0.0");
+        return swagger2Feature;
+    }
+}
---
+++ lib/apache-cxf-3.1.17/samples/jax_rs/tracing_brave/README.txt
@@ -0,0 +1,47 @@
+JAX-RS Brave/OpenZipkin Tracing Demo 
+=================
+
+The demo shows a basic usage of Brave/OpenZipkin distributed tracer with REST based 
+Web Services using  JAX-RS 2.0 (JSR-339). The REST server provides the 
+following services: 
+
+A RESTful catalog service is provided on URL http://localhost:9000/catalog 
+
+A HTTP GET request to URL http://localhost:9000/catalog generates following 
+traces:
+
+A HTTP POST request to URL http://localhost:9000/catalog generates following 
+traces:
+
+A HTTP GET request to URL http://localhost:9000/catalog/<id> generates following 
+traces:
+
+A HTTP DELETE request to URL http://localhost:9000/catalog/<id> generates following 
+traces:
+
+Building and running the demo using Maven
+---------------------------------------
+
+Run OpenZipkin server (or point CatalogTracing to the existing one): 
+
+  docker run -d -p 9411:9411 openzipkin/zipkin
+
+From the base directory of this sample (i.e., where this README file is
+located), the Maven pom.xml file can be used to build and run the demo. 
+
+
+Using either UNIX or Windows:
+
+  mvn install
+  mvn -Pserver  (from one command line window)
+  mvn -Pclient  (from a second command line window)
+    
+
+To remove the target dir, run mvn clean".
+
+Navigate to Zipkin UI to explore the traces (or point to existing deployment): 
+  
+  http://localhost:9411/ 
+
+
+
